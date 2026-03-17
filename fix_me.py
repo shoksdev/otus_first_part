920e2800d4c4742c36d3ce04d704d2ea74cb094b
maponomarev <maponomarev>
2026-03-17 09:09:44 +0000

    first_task - add docstrings, return to calculate_average function, rename variables

--- fix_me.py
+++ fix_me.py
@@ -1,7 +1,12 @@
-def calculate_average(nums):
-    total = sum(nums)
-    count = len(nums)
-    total / count
+"""Модуль содержащий функцию для расчета среднего значения"""
+
+
+def calculate_average(numbers: list):
+    """Функция для расчета среднего значения из переданного списка"""
+    total = sum(numbers)
+    count = len(numbers)
+    return total / count
+
 
 nums = [10, 15, 20]
 result = calculate_average(nums)
